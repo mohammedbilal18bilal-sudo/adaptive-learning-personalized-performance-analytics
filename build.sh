8b5b52686bc1dac25a8d9915ddf17b2c57f04114
Mohammed Bilal <mohammedbilal18.bilal@gmail.com>
2026-09-20 16:11:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py create_demo_users
+python manage.py seed_old_data
 python manage.py collectstatic --no-input
